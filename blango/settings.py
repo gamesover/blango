73ab6baee8e69f4bd524cbb6ca9655a0a349b77d
Ken Ding <dingwenhe@gmail.com>
2024-11-02 01:16:08 +0000

    Finish logging

--- blango/settings.py
+++ blango/settings.py
@@ -108,7 +108,27 @@ class Dev(Configuration):
         ),
     }
 
-
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+                "formatter": "verbose",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+    }
     # Password validation
     # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
